22bbf10ce3d5baa19caff50421c7ead72cd3b03f
Trubin Andrey <trubin23@gmail.com>
2019-02-22 23:28:23 +0500

    add databinding to project

---
+++ app/src/main/java/ru/trubin23/data_binding/Employee.java
@@ -0,0 +1,14 @@
+package ru.trubin23.data_binding;
+
+public class Employee {
+
+    public long id;
+    public String name;
+    public String address;
+
+    public Employee(long id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+}
--- app/src/main/java/ru/trubin23/data_binding/MainActivity.java
+++ app/src/main/java/ru/trubin23/data_binding/MainActivity.java
@@ -1,13 +1,22 @@
 package ru.trubin23.databinding;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ru.trubin23.data_binding.Employee;
+import ru.trubin23.databinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Employee employee = new Employee(1, "John Smith", "London");
+
+        ActivityMainBinding binding =
+                DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setEmployee(employee);
     }
 }
