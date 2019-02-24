fc4e43cf282b2751c1d112228d07eb0b16c924f0
Trubin Andrey <trubin23@gmail.com>
2019-02-24 13:48:14 +0500

    add more expressions to layout

--- app/src/main/java/ru/trubin23/data_binding/Employee.java
+++ app/src/main/java/ru/trubin23/data_binding/Employee.java
@@ -5,10 +5,12 @@ public class Employee {
     public long id;
     public String name;
     public String address;
+    public int salary;
 
-    public Employee(long id, String name, String address) {
+    public Employee(long id, String name, String address, int salary) {
         this.id = id;
         this.name = name;
         this.address = address;
+        this.salary = salary;
     }
 }
--- app/src/main/java/ru/trubin23/data_binding/MainActivity.java
+++ app/src/main/java/ru/trubin23/data_binding/MainActivity.java
@@ -3,7 +3,6 @@ package ru.trubin23.databinding;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import ru.trubin23.data_binding.Employee;
 import ru.trubin23.databinding.databinding.ActivityMainBinding;
@@ -14,33 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final Employee employee = new Employee(1, "John Smith", "London");
+        final Employee employee = new Employee(1, "John Smith", "London", 10_000);
 
         final ActivityMainBinding binding =
                 DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setEmployee(employee);
-
-        findViewById(R.id.second_employee).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Employee secondEmployee = new Employee(2, "Alexander", "Washington");
-                binding.setEmployee(secondEmployee);
-            }
-        });
-
-        findViewById(R.id.change_data).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                employee.name = "George";
-                employee.address = "Chester";
-            }
-        });
-
-        findViewById(R.id.invalidate_all).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                binding.invalidateAll();
-            }
-        });
     }
 }
