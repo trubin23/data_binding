3e9ad6aee7f634faad85da69dd482d024915b662
Trubin Andrey <trubin23@gmail.com>
2019-02-25 23:59:55 +0500

    add Map and List to layout

--- app/src/main/java/ru/trubin23/data_binding/MainActivity.java
+++ app/src/main/java/ru/trubin23/data_binding/MainActivity.java
@@ -3,6 +3,14 @@ package ru.trubin23.databinding;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import ru.trubin23.data_binding.Department;
 import ru.trubin23.data_binding.Employee;
@@ -17,9 +25,27 @@ public class MainActivity extends AppCompatActivity {
         Employee employee = new Employee(1, "John Smith", "London", 10_000);
         Department department = new Department(100, "IT");
 
+        List<String> hobbies = new ArrayList<>();
+        hobbies.add("Drawing");
+        hobbies.add("Photography");
+        hobbies.add("Learn a language");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("key 1", "value 1");
+        map.put("key 2", "value 2");
+        map.put("key 3", "value 3");
+
         final ActivityMainBinding binding =
                 DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setEmployee(employee);
         binding.setDepartment(department);
+        binding.setHobbies(hobbies);
+        binding.setMap(map);
+
+        TextView textView = binding.address;
+        View rootView = binding.getRoot();
+
+        Log.d("MainActivity", textView.toString());
+        Log.d("MainActivity", rootView.toString());
     }
 }
